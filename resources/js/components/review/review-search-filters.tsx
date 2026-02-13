fd563c1453f8809f45d32161f4935b8c00d53f6f
Nick Lang <nll5171@rit.edu>
2026-02-13 17:00:33 -0500

    Added very primitive filtering.

--- resources/js/components/review/review-search-filters.tsx
+++ resources/js/components/review/review-search-filters.tsx
@@ -1,19 +1,22 @@
 import { router } from '@inertiajs/react';
-import { useState } from 'react';
 
 export default function ReviewSearchFilters({ uid }: { uid: number }) {
-    const [getParams, setParams] = useState('');
+    // Make an object with three different fields, and change it stately
+    const initialParams = new URLSearchParams(window.location.search);
 
     function makeQuery(e) {
         const params = [];
 
-        if (document.querySelector('#user-submission-filter')!.checked)
-            params.push(`user_id=${uid}`);
-
-        if (document.querySelector('#for-filter')!.checked)
-            params.push(`status=0`);
-        else if (document.querySelector('#against-filter')!.checked)
-            params.push(`status=1`);
+        if (e.target.id === 'user-filter') {
+            if (e.target.checked) params.push(`user_id=${uid}`);
+            if (initialParams.get('status'))
+                params.push(`status=${initialParams.get('status')}`);
+        } else {
+            if (e.target.checked)
+                params.push(`status=${e.target.id === 'for-filter' ? 0 : 1}`);
+            if (initialParams.get('user_id'))
+                params.push(`user_id=${initialParams.get('user_id')}`);
+        }
 
         router.get(`/reviews?${params.join('&')}`);
     }
@@ -21,12 +24,14 @@ export default function ReviewSearchFilters({ uid }: { uid: number }) {
     return (
         <div className="flex items-center gap-2">
             <span>Filter by:</span>
-            <form className="flex gap-1" onChange={makeQuery}>
+            <form className="flex gap-1">
                 <input
                     className="btn"
                     type="checkbox"
                     name="filters"
-                    id="user-submission-filter"
+                    id="user-filter"
+                    checked={initialParams.get('user_id') !== null}
+                    onChange={makeQuery}
                     aria-label="User's submissions"
                 />
                 <input
@@ -34,6 +39,11 @@ export default function ReviewSearchFilters({ uid }: { uid: number }) {
                     type="checkbox"
                     name="filters"
                     id="for-filter"
+                    checked={
+                        initialParams.get('status') !== null &&
+                        parseInt(initialParams.get('status')) === 0
+                    }
+                    onChange={makeQuery}
                     aria-label="For"
                 />
                 <input
@@ -41,6 +51,11 @@ export default function ReviewSearchFilters({ uid }: { uid: number }) {
                     type="checkbox"
                     name="filters"
                     id="against-filter"
+                    checked={
+                        initialParams.get('status') !== null &&
+                        parseInt(initialParams.get('status')) === 1
+                    }
+                    onChange={makeQuery}
                     aria-label="Against"
                 />
             </form>
